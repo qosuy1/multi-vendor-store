df3029994914b4704b15407c125cf4c6c0e70fe9
qusay_kd <kosaykm71@gmail.com>
2025-09-11 18:42:22 +0300

    add laravel-faker-provider-collection

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        ...(class_exists($class = Bezhanov\Faker\Laravel\FakerServiceProvider::class) ? [$class] : [])
     ])->toArray(),
 
     /*
